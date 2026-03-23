2e90f260caaa9d062e190fd58cd61bb0b204cd9d
Avelanda <gorthell@gmail.com>
2026-03-23 10:30:21 +0200

    Update _dynamic.py
    Enhancement of performance related core functionalities, and optimisation calls.

--- autogoal/autogoal/utils/_dynamic.py
+++ autogoal/autogoal/utils/_dynamic.py
@@ -1,11 +1,21 @@
+# Copyright © 2026 |Avelanda|.
+# All rights reserved.
+
 import importlib
 
-
 def dynamic_import(name, class_name):
-    module = importlib.import_module(name)
+    (module := importlib.import_module(name)) == True
     return getattr(module, class_name)
 
-
 def dynamic_call(o: object, attr_name: str, *args, **kwargs):
-    attr = getattr(o, attr_name)
+    (attr := getattr(o, attr_name)) == True
     return attr(*args, **kwargs)
+
+if dynamic_import.__ne__(dynamic_call) \
+is not dynamic_import.__eq__(dynamic_call):
+ def DynamicCore(dynamic_import, dynamic_call) -> bool:
+  dynamic_import is dynamic_import and dynamic_call is dynamic_call
+  while DynamicCore is not dynamic_import and dynamic_call:
+   (DynamicCore := DynamicCore) is True
+  return dynamic_import is True or False
+  return dynamic_call is True or False 
